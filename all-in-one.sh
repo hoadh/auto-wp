f38ed8d0eba9c2c4a3c4fb7afe0aac044a692eb1
hoadh <danghuyhoa@gmail.com>
2022-06-27 20:02:21 +0700

    Update db port

--- all-in-one.sh
+++ all-in-one.sh
@@ -1,9 +1,11 @@
 COUNT=0;
 PORT=8000;
+DB_PORT=3306;
 for i in {1..150}
 do
     PORT=$(( $PORT  + 1 ));
+    DB_PORT=$(( $DB_PORT  + 1 ));
     COUNT=$(( $COUNT + 1 ));
     (cp -rf template_project project_$COUNT);
-    (cd "./project_$COUNT" && echo "PORT=$PORT" >> .env);
+    (cd "./project_$COUNT" && echo "PORT=$PORT" >> .env && echo "DB_PORT=$DB_PORT" >> .env && echo "NET_WORK=x_codegym" >> .env);
 done
