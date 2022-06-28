73bdfe4ffd727b61ce0da4c62313e63da5632e35
hoadh <danghuyhoa@gmail.com>
2022-06-28 14:51:11 +0700

    Fix error in port and db_port

--- all-in-one.sh
+++ all-in-one.sh
@@ -1,12 +1,12 @@
-COUNT=0;
 PORT=8000;
 DB_PORT=3306;
 
 for i in {1..15}
 do
-    PORT=$(( $PORT  + 1 ));
-    DB_PORT=$(( $DB_PORT  + 1 ));
-    COUNT=$(( $COUNT + 1 ));
-    (cp -rf template_project project_$COUNT);
-    (cd "./project_$COUNT" && echo "PORT=$PORT" >> .env && echo "DB_PORT=$DB_PORT" >> .env && echo "NET_WORK=x_codegym" >> .env && echo "VIRTUAL_HOST=project$COUNT.wp.warface.codegym.vn" >> .env);
+    CURRENT_PORT=$(( $PORT  + $i ));
+    CURRENT_DB_PORT=$(( $DB_PORT  + $i ));
+    PROJECT_NAME="project$i";
+    FOLDER_NAME="project_$i";
+    (cp -rf template_project $FOLDER_NAME);
+    (cd "./$FOLDER_NAME" && echo "PORT=$CURRENT_PORT" >> .env && echo "DB_PORT=$CURRENT_DB_PORT" >> .env && echo "NET_WORK=x_codegym" >> .env && echo "VIRTUAL_HOST=$PROJECT_NAME.wp.warface.codegym.vn" >> .env);
 done
